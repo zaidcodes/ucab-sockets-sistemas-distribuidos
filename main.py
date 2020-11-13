6744a87ea14cbae6275aea0adb10b0962bf32a2c
Julio <diaz.cesar.julio@gmail.com>
2020-11-13 13:56:15 -0400

    [FIX] Workflow program

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import sys
-import menu as menu
-import client as socketClient
+from client import *
 
 import os
 
@@ -10,26 +9,22 @@ def clear():
     else:
         os.system("clear")
 
-op = -1
-client = socketClient.Client("10.2.126.2", 19876 )
+try:
+    ip_server = sys.argv[1]
+    port_server = sys.argv[2]
+    username = sys.argv[3]
 
-while op != 0:
-    clear()
-    menu.mainMenu(client.nombre)
-    op = input("Opción: ")
-    if op == "1":
-        if not client.isLogged():
-            usuario = input("Usuario: ")
-            client.login(usuario)
-        else:
-            client.logout()
-    elif op == "2":
-        print("nei")
-    elif op == "3":
-        print("nei")
-    elif op == "4":
-        print("nei")
-    elif op == "5":
-        print("nei")
-    elif op == "0":
-        break
+    client = Client(ip_server, port_server)
+    client.hello(username)
+    client.msglen()
+    client.givememsg()
+    client.chkmsg()
+    client.bye()
+    # clear()
+    print("*"*7, "Su mensaje es", "*"*7, "\n\n")
+    print(" "*4, client.message)
+    print("\n\n")
+except Exception as e:
+    print("Vuelva a internarlo nuevamente. [", e,"]")
+finally:
+    print("\nHasta luego\n")
