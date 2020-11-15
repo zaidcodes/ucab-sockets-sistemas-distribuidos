cbdd88cee3dee7871c93424849e7c0f59ea85a60
Julio <diaz.cesar.julio@gmail.com>
2020-11-14 21:05:21 -0400

    [ADD] Cheking message is correct loop

--- main.py
+++ main.py
@@ -17,10 +17,20 @@ try:
     client = Client(ip_server, port_server)
     client.hello(username)
     client.msglen()
-    client.givememsg()
-    client.chkmsg()
+    correct_message = False
+    cont = 0
+    print("Bienvenido {username}, estamos recuperando tu mensaje...".format(username = username) )
+    while correct_message == False:
+        client.givememsg()
+        chkmsg = client.chkmsg()
+        if chkmsg.find("ok") != -1:
+            correct_message = True
+        else:
+            cont = cont + 1
+        if cont >= 3:
+            raise MaxAttemptsCorrectMessageException(cont)
     client.bye()
-    # clear()
+    clear()
     print("*"*7, "Su mensaje es", "*"*7, "\n\n")
     print(" "*4, client.message)
     print("\n\n")
